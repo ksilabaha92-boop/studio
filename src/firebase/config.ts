a2da2c3c4922243f44bd6638910384a34f943d2e
Baha Ksila <ksilabaha92@gmail.com>
2026-04-25 21:51:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8523947124-ccfc9",
+  "appId": "1:861271033277:web:cf284623f8adff3b293ed4",
+  "apiKey": "AIzaSyDUj6kSJFpqOnKYbgoBr1h12ytgR7r3Uzw",
+  "authDomain": "studio-8523947124-ccfc9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "861271033277"
+};
